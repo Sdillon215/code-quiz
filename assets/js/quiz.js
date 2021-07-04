bf041dadcc044ea0ed0404c7997baeab2d1e889b
Sean <sdillon215@gmail.com>
2021-07-03 17:55:44 -0700

    timer added

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,8 +1,21 @@
 var startBtnEl = document.getElementById("start-btn");
+var timeLeft = document.getElementById("time-left").innerText = 60;
 
+
+
+
+// function to hide start screen on click of start-btn
 var startHide = function (event) {
     document.getElementById("start-hide").style.display = "none";
-    console.log("clicked");
+
+    // Timer function called by startHide function on start click to start the timer for the quiz
+    var quizTime = setInterval(function() {
+        if(timeLeft <= 0) {
+            clearInterval(quizTime);
+        }
+        document.getElementById("time-left").innerText = timeLeft;
+        timeLeft--;
+    }, 1000);
 };
 
 startBtnEl.addEventListener("click", startHide);
